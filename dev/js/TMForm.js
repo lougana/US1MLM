25cdbf76d68ffb16c914ff8d915d0159eae5ad0a
lougana <lmaganmbox@gmail.com>
2016-07-30 22:03:00 -0400

    Contact: changed to my email for testing

--- dev/js/TMForm.js
+++ dev/js/TMForm.js
@@ -2,23 +2,23 @@
 (function($){
 	$.fn.TMForm=function(opt){
 		return this.each(TMForm)
-		
+
 		function TMForm(){
 			var form=$(this)
-			opt=$.extend({					
+			opt=$.extend({
 					okClass:'ok'
 					,emptyClass:'empty'
 					,invalidClass:'invalid'
 					,successClass:'success'
 					,onceVerifiedClass:'once-verified'
-					,mailHandlerURL:'bat/MailHandler.php'					
+					,mailHandlerURL:'bat/MailHandler.php'
 					,successShowDelay:'4000'
 					,stripHTML:true
 				},opt)
-				
+
 			init()
-			
-			function init(){				
+
+			function init(){
 				form
 					.on('submit',formSubmit)
 					.on('reset',formReset)
@@ -30,10 +30,10 @@
 							.addClass(opt.onceVerifiedClass)
 							.trigger('validate.form')
 					})
-					.on('keyup','[data-constraints].once-verified',function(){						
+					.on('keyup','[data-constraints].once-verified',function(){
 						$(this).trigger('validate.form')
-					})					
-				
+					})
+
 				if($('[data-constraints]',form).length!==0)
 					$('[data-constraints]',form)
 						.regula('bind')
@@ -41,62 +41,62 @@
 							fieldDesolation($(this))
 						})
 						.on('validate.form',fieldValidate)
-					
-				
+
+
 				$('[placeholder]',form).TMPlaceholder()
-				
+
 				$('[data-type=submit]',form)
-					.click(function(){						
+					.click(function(){
 						form.trigger('submit')
 						return false
 					})
-					
+
 				$('[data-type=reset]',form)
-					.click(function(){						
+					.click(function(){
 						form.trigger('reset')
 						return false
 					})
 			}
-			
+
 			function fieldValidate(el){
 				var el=$(this)
 					,result=el.regula('validate')
 					,isEmpty=false
 					,isInvalid=false
 					,isRequired=!!~el.data('constraints').indexOf('@Required')
-				
+
 				$.each(result,function(){
 					if(this.constraintName==='Required')
 						isEmpty=true
 					else
 						isInvalid=true
 				})
-				
+
 				if(!el.hasClass(opt.onceVerifiedClass)&&!isEmpty)
 					el.addClass(opt.onceVerifiedClass)
-					
+
 				if(isEmpty)
 					el.parents('label').addClass(opt.emptyClass)
-								
+
 				if(isInvalid&&!isEmpty&&isRequired)
 					el.parents('label')
 						.removeClass(opt.emptyClass)
 						.removeClass(opt.okClass)
 						.addClass(opt.invalidClass)
-						
+
 				if(isInvalid&&!isRequired&&el.val())
 					el.parents('label')
 					.removeClass(opt.emptyClass)
 					.removeClass(opt.okClass)
 					.addClass(opt.invalidClass)
-					
+
 				if(!result.length)
 					el.parents('label')
 						.removeClass(opt.invalidClass)
 						.removeClass(opt.emptyClass)
 						.addClass(opt.okClass)
 			}
-			
+
 			function fieldDesolation(el){
 				el
 					.removeClass(opt.onceVerifiedClass)
@@ -105,15 +105,15 @@
 						.removeClass(opt.emptyClass)
 						.removeClass(opt.okClass)
 			}
-			
+
 			function getValue(el){
 				return el.val()||false
 			}
-			
+
 			function formSubmit(){
 				$('[data-constraints]',form).trigger('validate.form')
-				
-				if(!$('label.'+opt.invalidClass+',label.'+opt.emptyClass,form).length){					
+
+				if(!$('label.'+opt.invalidClass+',label.'+opt.emptyClass,form).length){
 					$.ajax({
 						type:"POST"
 						,url:opt.mailHandlerURL
@@ -124,10 +124,10 @@
 							,fax:getValue($('label.fax input'))
 							,state:getValue($('label.state input'))
 							,message:getValue($('label.message textarea'))
-							,owner_email:opt.ownerEmail||'#'
+							,owner_email:opt.ownerEmail||'louweezjr@gmail.com'
 							,stripHTML:opt.stripHTML
 						}
-						,success: function(e){							
+						,success: function(e){
 							form.addClass(opt.successClass)
 							setTimeout(function(){
 								form
@@ -135,16 +135,16 @@
 								.trigger('reset')
 							},opt.successShowDelay)
 						}
-					})					
-				}				
+					})
+				}
 				return false
 			}
-			
+
 			function formReset(){
-				fieldDesolation($('[data-constraints]',form))					
+				fieldDesolation($('[data-constraints]',form))
 			}
 		}
-	}	
+	}
 })(jQuery)
 
 ;(function($){
@@ -153,36 +153,36 @@
 			var th=$(this)
 				,placeholder_text
 				,placeholder
-						
+
 			opt=$.extend({
 					placeholderClass:'_placeholder'
 					,placeholderFocusedClass:'focused'
 					,placeholderHiddenClass:'hidden'
 				},opt)
-				
+
 			init()
-			
+
 			function init(){
 				placeholder_text=th.attr('placeholder')
 				placeholder=$(document.createElement('span'))
 				placeholder
 					.addClass(opt.placeholderClass)
-					.css({				
+					.css({
 						left:th.prop('offsetLeft')
 						,top:th.prop('offsetTop')
 						,width:th.width()
-						,height:th.outerHeight()				
+						,height:th.outerHeight()
 					})
 					.text(placeholder_text)
-					.appendTo(th.parent())				
+					.appendTo(th.parent())
 					.click(function(){
 						th.focus()
 						return false
 					})
-					.on('contextmenu',function(){						
-						th.trigger('hide.placeholder').focus()						
+					.on('contextmenu',function(){
+						th.trigger('hide.placeholder').focus()
 					})
-				
+
 				th
 					.val('')
 					.removeAttr('placeholder')
@@ -198,26 +198,26 @@
 					.on('blur',function(){
 						var val=th.val()
 						if(val===''||val===placeholder_text)
-							th.val('')							
+							th.val('')
 							,th.trigger('show.placeholder')
 						placeholder.removeClass(opt.placeholderFocusedClass)
 					})
-					.on('keydown',function(e){												
+					.on('keydown',function(e){
 						if(e.keyCode===32||e.keyCode>46)
-							th.trigger('hide.placeholder')							
+							th.trigger('hide.placeholder')
 					})
-					.on('keyup',function(){						
-						if(th.val()===''){							
+					.on('keyup',function(){
+						if(th.val()===''){
 							th.trigger('show.placeholder')
 							return false
-						}else{							
+						}else{
 							th.trigger('hide.placeholder')
 						}
 					})
 					.parents('form').on('reset',function(){
-						th.trigger('show.placeholder')						
+						th.trigger('show.placeholder')
 					})
-			}			
+			}
 		})
 	}
 })(jQuery)
@@ -252,10 +252,10 @@ regula.custom({
 })
 $(window).load(function(){
         $('#contact-form').TMForm({
-            ownerEmail:'#' 
+            ownerEmail:'louweezjr@gmail.com'
         })
         $('#contact-form2').TMForm({
-            ownerEmail:'#' 
+            ownerEmail:'louweezjr@gmail.com'
         })
 
     })
