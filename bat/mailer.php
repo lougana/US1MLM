d3f22f8e76a213d4eb2e10e6d45255e9d753ad60
lougana <lmaganmbox@gmail.com>
2016-08-04 14:51:33 -0400

    Mailer: removed variables not in use like message and name for subscription

--- bat/mailer.php
+++ bat/mailer.php
@@ -6,10 +6,7 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -24,12 +21,10 @@
         $recipient = "luis@brandster.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "New Music Subscriber from US1MLM";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
